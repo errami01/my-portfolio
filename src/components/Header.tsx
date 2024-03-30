f904926cd5dd9e873da96d48efafc46e47b92006
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-03-30 12:02:20 +0000

    Add bars icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 const Header = ()=>{
     return(
-        <header>
-            <h1 className="text-3xl font-bold underline fle">
-                Hello my Header
+        <header className="bg-red-200">
+            <h1 className="text-3xl font-bold ">
+                Logo
             </h1>
+            <div>
+                <i className="fa-solid fa-bars text-3xl"></i>
+            </div>
         </header>
     )
 }
