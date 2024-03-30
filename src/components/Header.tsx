925b3417985972dac95ec49c3f68f341243c5f10
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-03-30 10:58:04 +0000

    Create components folder and Header component module

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = ()=>{
+    return(
+        <header>
+            <h1 className="text-3xl font-bold underline fle">
+                Hello my Header
+            </h1>
+        </header>
+    )
+}
+
+export default Header
