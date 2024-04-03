a2e49f281b641a3142e0fd516239eda18cc18f95
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-03 11:24:57 +0000

    Add nav bar items and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 const Header = ()=>{
     return(
-        <header className="bg-red-200">
-            <h1 className="text-3xl font-bold ">
+        <header className="flex justify-between h-[80px] px-[50px] shadow-[0_0_10px_rgba(0,0,0,.09)]">
+            <h1 className="flex items-center text-3xl font-bold ">
                 Logo
             </h1>
-            <div>
-                <i className="fa-solid fa-bars text-3xl"></i>
+            <div className="flex  text-[1.7rem]">
+                <i className="fa-solid fa-bars md:hidden  self-center text-[2.3rem]"></i>
+                <div className="hidden md:flex gap-[2rem] font-[600] ">
+                    <a href="#" className=" flex items-center ">Home</a>
+                    <a href="#" className=" flex items-center">Projects</a>
+                    <a href="#" className="flex items-center">contact</a>
+                </div>
             </div>
         </header>
     )
