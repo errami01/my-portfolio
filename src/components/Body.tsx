bcd15414ccede82b4774bc9f391ca1b189574853
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-05 11:26:18 +0000

    Add styles const to hold classNames

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,11 +1,14 @@
 const Body = ()=>{
+    const styles ={
+        profileImage:`bg-[url('./assets/abdellatif.jpg')] size-[35rem] bg-[length:110%]
+        bg-center bg-no-repeat border-8`,
+    }
     return(
         <div>
-            <section>
-                <div></div>
+            <section >
+                <div className={styles.profileImage}></div>
             </section>
         </div>
     )
 }
-
 export default Body
