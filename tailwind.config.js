061f9f44918c349f3bbc13f9e982a8fff26b02d8
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-05 11:09:36 +0000

    Set the configuration for borderRadius animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        borderRadius: {
+          '0%, 100%': { 'border-radius': '40% 80% 40% 60%' },
+          '50%': { 'border-radius': '60% 40% 50% 35%' }
+        }
+      },
+      animation: {
+        borderRadius: 'borderRadius 15s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
