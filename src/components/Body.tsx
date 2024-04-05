62b108c8dfa4b243aa97cbe4ae366d2ca4cd1c02
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-05 11:27:39 +0000

    Add animate-borderRadius className to profile image element

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,7 +1,7 @@
 const Body = ()=>{
     const styles ={
         profileImage:`bg-[url('./assets/abdellatif.jpg')] size-[35rem] bg-[length:110%]
-        bg-center bg-no-repeat border-8`,
+        bg-center bg-no-repeat border-8 animate-borderRadius`,
     }
     return(
         <div>
