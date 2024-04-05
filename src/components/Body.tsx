6524ae2d1cef5fdfde436d3dcdfb5ddac34b6aa4
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-05 15:37:19 +0000

    Reduce photo size on mobile screen and add transition to it on screen resize

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,12 +1,15 @@
 const Body = ()=>{
     const styles ={
-        profileImage:`bg-[url('./assets/abdellatif.jpg')] size-[35rem] bg-[length:110%]
-        bg-center bg-no-repeat border-8 animate-borderRadius`,
+        homeSection:`border-8 flex flex-col items-center gap-[3rem]`,
+        profileImage:`bg-[url('./assets/abdellatif.jpg')] transition-size duration-1000 size-[28rem] md:size-[35rem]
+        bg-[length:110%] bg-center bg-no-repeat border-8 animate-borderRadius`,
+        title: `text-6xl text-center font-bold `
     }
     return(
         <div>
-            <section >
+            <section className={styles.homeSection}>
                 <div className={styles.profileImage}></div>
+                <h1 className={styles.title}>Front-End React Developer</h1>
             </section>
         </div>
     )
