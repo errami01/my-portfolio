df8b11974841fc07ab67ca707f77e6c39d901b8c
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-08 12:10:48 +0000

    Set flex-basis for titleToGithubContainer

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,6 +6,7 @@ const Body = ()=>{
         profileImage:`bg-[url('./assets/abdellatif.jpg')] transition-size duration-1000
         size-[28rem] md:size-[35rem] bg-[length:110%] bg-center bg-no-repeat border-8 
         animate-borderRadius`,
+        titleToGithubContainer:`basis-[50rem]`,
         title: `text-6xl font-bold `,
         heroDescription: `text-[1.8rem] font-medium text-[#555] mt-10`,
         iconContainer:`text-[3rem]  flex gap-10 justify-center`
@@ -15,7 +16,7 @@ const Body = ()=>{
             <section className={styles.homeSection}>
                 <div className={styles.imageAndText}>
                     <div className={styles.profileImage}></div>
-                    <div>
+                    <div className={styles.titleToGithubContainer}>
                         <h1 className={styles.title}>Front-End React Developer</h1>
                         <p className={styles.heroDescription}>
                             Hi, I'm Abdellatif Errami. A passionate Front-end React
