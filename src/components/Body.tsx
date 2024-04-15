284735b43f1c4ad812486203279e0cc6ab821e80
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-15 12:20:54 +0100

    Render Projects component inside Body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,10 +1,12 @@
 import Hero from "./Hero"
+import Projects from "./Projects"
 
 const Body = ()=>{
     
     return(
         <div className="p-8 max-w-[107rem] m-auto">
            <Hero /> 
+           <Projects />
         </div>
     )
 }
