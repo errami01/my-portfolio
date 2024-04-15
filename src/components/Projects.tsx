1177e90bd58cb5d7ea4e3dd3220fdf495b00dcf0
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-15 15:54:37 +0100

    Render 4 ProjectBox components inside Projects container

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,12 @@
+import ProjectBox from "./ProjectBox"
+
 const Projects = ()=>{
     return(
         <section>
+            <ProjectBox />
+            <ProjectBox />
+            <ProjectBox />
+            <ProjectBox />
         </section>
     )
 }
