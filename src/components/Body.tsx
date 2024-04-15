8c2dcd26064079e11925eec4b4d6e54c1b5604a3
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-16 00:21:13 +0100

    Set Body container as flex and set the gap and top margin

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -4,7 +4,7 @@ import Projects from "./Projects"
 const Body = ()=>{
     
     return(
-        <div className="p-8 max-w-[107rem] m-auto">
+        <div className="p-8 max-w-[107rem] m-auto flex flex-col gap-[10rem] mt-[5rem]">
            <Hero /> 
            <Projects />
         </div>
