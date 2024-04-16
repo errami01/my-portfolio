48f8a8f1eed098d0d3a8f5c9705c02ed3061f941
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-16 10:46:44 +0100

    Move ProjectBox component inside a div and set it as grid element

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,16 @@ import ProjectBox from "./ProjectBox"
 
 const Projects = ()=>{
     const styles ={
-        
+        projects: `grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-[5rem]`
     }
     return(
-        <section>
-            <ProjectBox />
-            <ProjectBox />
-            <ProjectBox />
-            <ProjectBox />
+        <section >
+            <div className={styles.projects}>
+                <ProjectBox />
+                <ProjectBox />
+                <ProjectBox />
+                <ProjectBox />
+            </div>
         </section>
     )
 }
