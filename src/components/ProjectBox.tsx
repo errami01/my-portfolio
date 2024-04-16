c34dc0097ea21383782eb32bdb13aa3b8b5c5cc4
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-17 00:37:55 +0100

    Remove height and add gap to the container

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -3,8 +3,8 @@ type ProjectBoxProps = {
 }
 const ProjectBox = ({imageSource}:ProjectBoxProps)=>{
     const styles ={
-        container:`flex flex-col items-center h-[53rem] border-[0.3rem] bg-white rounded-[2.5rem]
-        p-[3rem]`
+        container:`flex flex-col items-center gap-[2rem]  border-[0.3rem] bg-white rounded-[2.5rem]
+        p-[3rem] text-[1.6rem]`
     }
     return(
         <div className={styles.container}>
