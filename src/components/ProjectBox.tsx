f1cd1ca3d5f7fecdec9ef94cf2603e17380c7c7b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-16 19:12:26 +0100

    Add img element and a p element with placeholder text for description

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -1,10 +1,17 @@
-const ProjectBox = ()=>{
+type ProjectBoxProps = {
+    imageSource: string
+}
+const ProjectBox = ({imageSource}:ProjectBoxProps)=>{
     const styles ={
         container:`h-[53rem] border-4`
     }
     return(
         <div className={styles.container}>
-
+            <img src={imageSource} width="310"  alt="Project image" />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Odio, repellendus. Incidunt, laboriosam distinctio temporibus, 
+                adipisci soluta modi id ut molestiae quidem optio asperiores a. 
+                Delectus assumenda provident veniam dolorem.</p>
         </div>
     )
 }
