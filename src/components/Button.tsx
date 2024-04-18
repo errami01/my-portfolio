8c7ffceddbf4877b5a8c8ad721e3557d9c26c940
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-18 16:22:11 +0100

    Add styles to button text and change its background color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,8 @@ type ButtonProps = {
 const Button = ({text}: ButtonProps)=>{
 
     return(
-        <button className="w-[14.5rem] h-[4.5rem] bg-[#96cbeb]">{text}</button>
+        <button className="w-[14.5rem] h-[4.5rem] bg-[#4A86F7] rounded-[0.5rem]
+        text-white font-[600]">{text}</button>
     )
 }
 export default Button
