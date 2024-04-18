884af56d56816f6b94c68f91a811d0b0e8a8e4a6
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-18 12:08:32 +0100

    Add style for project description text

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -7,7 +7,8 @@ const ProjectBox = ({imageSource, projectName}:ProjectBoxProps)=>{
         container:`flex flex-col items-center gap-[2rem]  border-[0.3rem] bg-white rounded-[2.5rem]
         p-[3rem] text-[1.6rem]`,
         projectImage: `rounded-[1.5rem] z-[1]`,
-        projectName: `self-start font-bold text-[2.5rem]`
+        projectName: `self-start font-bold text-[2.5rem]`,
+        projectDescription: `text-[1.6rem] font-[500] text-[#555]`
     }
     return(
         <div className={styles.container}>
@@ -16,7 +17,7 @@ const ProjectBox = ({imageSource, projectName}:ProjectBoxProps)=>{
                 <div className="absolute w-[80%] bg-[#D3DEE5] h-[80%] rounded-[50%] blur-[5rem]"></div>
             </div>
             <h2 className={styles.projectName}>{projectName}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            <p className={styles.projectDescription}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Odio, repellendus. Incidunt, laboriosam distinctio temporibus, 
                 adipisci soluta modi id ut molestiae quidem optio asperiores a. 
                 Delectus assumenda provident veniam dolorem.</p>
