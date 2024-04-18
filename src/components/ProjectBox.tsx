03f7dfc3f0cb2392886e564ab98aaed9343cf3e0
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-18 10:28:51 +0100

    Add blur behind project image

--- src/components/ProjectBox.tsx
+++ src/components/ProjectBox.tsx
@@ -6,12 +6,15 @@ const ProjectBox = ({imageSource, projectName}:ProjectBoxProps)=>{
     const styles ={
         container:`flex flex-col items-center gap-[2rem]  border-[0.3rem] bg-white rounded-[2.5rem]
         p-[3rem] text-[1.6rem]`,
-        projectImage: `rounded-[1.5rem]`,
+        projectImage: `rounded-[1.5rem] z-[1]`,
         projectName: `self-start font-bold text-[2.5rem]`
     }
     return(
         <div className={styles.container}>
-            <img className={styles.projectImage} src={imageSource} width="310"  alt="Project image" />
+            <div className="relative flex items-center justify-center  w-[100%] h-[30rem]">
+                <img className={styles.projectImage} src={imageSource} width="310"  alt="Project image" />
+                <div className="absolute w-[80%] bg-[#D3DEE5] h-[80%] rounded-[50%] blur-[5rem]"></div>
+            </div>
             <h2 className={styles.projectName}>{projectName}</h2>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Odio, repellendus. Incidunt, laboriosam distinctio temporibus, 
