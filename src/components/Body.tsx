5347c151e7ea5eb70d7ca918bc6b2cb9c1991fb0
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-18 20:09:52 +0100

    Render Contact component inside Body container

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,3 +1,4 @@
+import Contact from "./Contact"
 import Hero from "./Hero"
 import Projects from "./Projects"
 
@@ -7,6 +8,7 @@ const Body = ()=>{
         <div className="p-8 max-w-[107rem] m-auto flex flex-col gap-[10rem] mt-[5rem]">
            <Hero /> 
            <Projects />
+           <Contact />
         </div>
     )
 }
