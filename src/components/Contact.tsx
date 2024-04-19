d093b9bf8fe917cb5d122a4412fc0b82c01948f6
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-19 10:51:21 +0100

    Render Contact component inside Body container

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,12 @@
 const Contact = ()=>{
+    const styles = {
+        icon:`text-[4rem] text-[#4A86F7]`
+    }
     return(
-        <section>
-            
+        <section className="bg-red-100">
+            <span><i className={`fa-solid fa-envelope ${styles.icon}`}></i></span>
+            <span><i className={`fa-solid fa-phone-volume ${styles.icon}`}></i></span>
+            <span><i className={`fa-solid fa-location-dot ${styles.icon}`}></i></span>
         </section>
     )
 }
