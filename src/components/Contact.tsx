2afa79b47fc97b9492d37fa6b49a31f75bd92891
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-19 11:33:50 +0100

    Render SectionTitle inside section element

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,12 @@
+import SectionTitle from "./SectionTitle"
+
 const Contact = ()=>{
     const styles = {
         icon:`text-[4rem] text-[#4A86F7]`
     }
     return(
         <section className="bg-red-100">
+            <SectionTitle text="Contact"/>
             <span><i className={`fa-solid fa-envelope ${styles.icon}`}></i></span>
             <span><i className={`fa-solid fa-phone-volume ${styles.icon}`}></i></span>
             <span><i className={`fa-solid fa-location-dot ${styles.icon}`}></i></span>
