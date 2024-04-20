2d773e3d54100150b9b15a2025bf2308efb59b70
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-20 15:57:21 +0100

    Add styles const and replace icon prop by fontAwsomeClass prop

--- src/components/ContactBox.tsx
+++ src/components/ContactBox.tsx
@@ -1,11 +1,16 @@
 type ContactBoxProps ={
-    icon: JSX.Element
+    fontAwsomeClass: string 
     contactInfo: string
 }
-const ContactBox = ({icon, contactInfo}: ContactBoxProps)=>{
+const ContactBox = ({fontAwsomeClass, contactInfo}: ContactBoxProps)=>{
+    const styles = {
+        icon:`text-[4rem] text-[#4A86F7]`,
+        iconContainer: `flex bg-white grow gap-[3rem] rounded-[1rem] p-[3rem] items-center 
+        text-[1.8rem] md:flex-col font-[500] text-[#555]`
+    }
     return(
         <div>
-            {icon}
+            <i className={`${fontAwsomeClass} ${styles.icon}`}></i>
             <p>{contactInfo}</p>
         </div>    
     )
