2536eba9e32c958f76bac61fff18d6e413eb4819
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 14:47:37 +0100

    Add link and fontAwsome props

--- src/components/SocialIcon.tsx
+++ src/components/SocialIcon.tsx
@@ -1,6 +1,11 @@
-const SocialIcon = ()=>{
+type SocialIconProps ={
+    link: string
+    fontAwsomeClass: string
+}
+const SocialIcon = ({link, fontAwsomeClass}: SocialIconProps)=>{
     return(
-     <a>
+     <a href={link} target="_blank">
+        <i className={fontAwsomeClass}></i>
      </a>   
     )
 }
