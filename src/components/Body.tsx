f3400bb0d756ca29fc9dafe54203c739ea9d2e26
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 16:22:21 +0100

    Add bottom margin to Body container

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,7 +5,7 @@ import Projects from "./Projects"
 const Body = ()=>{
     
     return(
-        <div className="p-8 max-w-[107rem] m-auto flex flex-col gap-[10rem] mt-[5rem]">
+        <div className="px-8 max-w-[107rem] m-auto flex flex-col gap-[10rem] my-[6rem]">
            <Hero /> 
            <Projects />
            <Contact />
