ef0cd70fef8dc47d513ee6607fe3c7f5684b9020
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 14:24:57 +0100

    Change div padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = ()=>{
     return(
         <footer className="bg-[#8b90a3] p-[5rem_0]">
-            <div className="max-w-[107rem] m-auto mt-[1px] p-8">
+            <div className="max-w-[107rem] m-auto mt-[1px] px-8">
                 <p className="text-white text-[1.8rem] font-[600]">Copyright © 2024. All rights are reserved</p>
             </div>
         </footer>
