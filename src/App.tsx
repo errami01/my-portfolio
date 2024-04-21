6109d3a46c0fd8e16ee3e1f2550523356752ce89
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 12:45:29 +0100

    Render Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Header from './components/Header'
 import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Body />
+      <Footer />
     </>
   )
 }
