9c20ce554537ffdddff45560f9af9d7afeab1821
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 16:15:35 +0100

    Render two SocialIcon component in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,20 @@
+import SocialIcon from "./SocialIcon"
+
 const Footer = ()=>{
     return(
         <footer className="bg-[#8b90a3] p-[5rem_0]">
-            <div className="max-w-[107rem] m-auto mt-[1px] px-8">
+            <div className="flex items-center justify-between max-w-[107rem] m-auto mt-[1px] px-8">
                 <p className="text-white text-[1.8rem] font-[600]">Copyright © 2024. All rights are reserved</p>
+                <div className={"text-[3rem] flex gap-10 text-white"}>
+                        <SocialIcon 
+                            link="https://www.linkedin.com/in/abdellatif-e-98513119a/"
+                            fontAwsomeClass="fa-brands fa-linkedin" 
+                        />
+                        <SocialIcon 
+                            link="https://github.com/errami01"
+                            fontAwsomeClass="fa-brands fa-github" 
+                        />
+                    </div>
             </div>
         </footer>
     )
