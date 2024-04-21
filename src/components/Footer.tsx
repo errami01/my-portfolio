7115f40e72e60fa8faae806181e84238f7980ed8
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-21 13:12:35 +0100

    Nest a div inside footer and set its max-width, margin, and padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = ()=>{
     return(
         <footer className="bg-[#8b90a3] p-[5rem_0]">
-            
+            <div className="max-w-[107rem] m-auto mt-[1px] p-8">
+                
+            </div>
         </footer>
     )
 }
