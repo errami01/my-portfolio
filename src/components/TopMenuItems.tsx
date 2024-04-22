2a6be0adac071ad533e69f2f102fc4710012044b
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-22 11:26:59 +0100

    Add menu items and containerClassName prop

--- src/components/TopMenuItems.tsx
+++ src/components/TopMenuItems.tsx
@@ -1,6 +1,13 @@
-const TopMenuItems = ()=>{
+type TopMenuItemsProps ={
+    containerClassName: string
+}
+const TopMenuItems = ({containerClassName}:TopMenuItemsProps)=>{
     return(
-      <div></div>  
+      <div className={containerClassName}>
+        <a href="#hero" className="flex items-center ">Home</a>
+        <a href="#projects" className="flex items-center ">Projects</a>
+        <a href="#" className="flex items-center">contact</a>
+      </div>
     )
 }
 export default TopMenuItems
