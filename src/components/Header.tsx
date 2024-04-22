44fd725cf5306d57c97b5ced74431ddd9c1ebb5f
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-22 11:46:09 +0100

    Render TopMenuItems and remove replaced code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import TopMenuItems from "./TopMenuItems"
+
 const Header = ()=>{
     return(
         <header className="flex justify-between h-[80px] px-[50px] shadow-[0_0_10px_rgba(0,0,0,.09)]">
@@ -6,11 +8,7 @@ const Header = ()=>{
             </h1>
             <div className="flex  text-[1.7rem]">
                 <i className="fa-solid fa-bars md:hidden  self-center text-[2.3rem]"></i>
-                <div className="hidden md:flex gap-[2rem] font-[600] ">
-                    <a href="#hero" className="flex items-center ">Home</a>
-                    <a href="#projects" className="flex items-center ">Projects</a>
-                    <a href="#" className="flex items-center">contact</a>
-                </div>
+                <TopMenuItems containerClassName="hidden md:flex gap-[2rem] font-[600]"/>
             </div>
         </header>
     )
