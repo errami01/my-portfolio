0d73f60cb463f94246af508e5b15639772e8407e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 12:28:10 +0100

    Move styles to styles object

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -1,9 +1,15 @@
 import TopMenuItems from "./TopMenuItems"
 
 const BarsMenu = () =>{
+    const styles={
+        container: `flex justify-center fixed top-0 bottom-0 left-0 right-0 z-[2]
+        bg-white md:hidden`,
+        topMenuItems:`flex items-center flex-col gap-[4rem] font-[600] mt-[15rem]
+        text-[2rem]`
+    }
     return (
-        <div className="flex justify-center fixed top-0 bottom-0 left-0 right-0 z-[2] bg-white md:hidden">
-            <TopMenuItems containerClassName=" flex items-center flex-col gap-[4rem] font-[600] mt-[15rem] text-[2rem]"/>
+        <div className={styles.container}>
+            <TopMenuItems containerClassName={styles.topMenuItems}/>
         </div>
     )
 }
