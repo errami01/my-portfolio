932d4858fc27fd9cead2708aa82c446b9fcc47e0
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 11:48:51 +0100

    Add styles for BarsMenu container

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -2,7 +2,7 @@ import TopMenuItems from "./TopMenuItems"
 
 const BarsMenu = () =>{
     return (
-        <div>
+        <div className="flex justify-center fixed top-0 bottom-0 left-0 right-0 z-[2] bg-white md:hidden">
             <TopMenuItems containerClassName=" flex items-center flex-col gap-[4rem] font-[600] mt-[15rem] text-[2rem]"/>
         </div>
     )
