369390e917ddab09cc09fa75c4902665fdb81ddd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 19:33:56 +0100

    Add barsMenu animation to the container

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -2,8 +2,8 @@ import TopMenuItems from "./TopMenuItems"
 
 const BarsMenu = () =>{
     const styles={
-        container: `flex justify-center fixed top-0 bottom-0 left-0 right-0 z-[2]
-        bg-white md:hidden`,
+        container: `flex justify-center fixed top-0 bottom-0 right-0 left-0 animate-barsMenu z-[2] 
+        bg-white md:hidden `,
         topMenuItems:`flex items-center flex-col gap-[4rem] font-[600] mt-[15rem]
         text-[2rem]`
     }
