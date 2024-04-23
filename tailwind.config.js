5d191e51cd6b4f9b376c354b550a9c7a266495bd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 19:29:13 +0100

    Implement barsMenu animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,15 @@ export default {
         borderRadius: {
           '0%, 100%': { 'border-radius': '40% 80% 40% 60%' },
           '50%': { 'border-radius': '60% 40% 50% 35%' }
+        },
+        barsMenuSlider:{
+          '0%':{'right': '100%', 'left': '-100%'},
+          '100%':{'right': '0', 'left': '0'}
         }
       },
       animation: {
         borderRadius: 'borderRadius 15s ease-in-out infinite',
+        barsMenu: 'barsMenuSlider 2s'
       }
     },
   },
