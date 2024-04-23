b401166f9d15f5641ce27d82e16ed99ec99a64cb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 11:41:25 +0100

    Render TopMenuItems component

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -1,7 +1,9 @@
+import TopMenuItems from "./TopMenuItems"
+
 const BarsMenu = () =>{
     return (
         <div>
-            
+            <TopMenuItems containerClassName=" flex items-center flex-col gap-[4rem] font-[600] mt-[15rem] text-[2rem]"/>
         </div>
     )
 }
