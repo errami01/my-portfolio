ae4837f78b93ebb516c7a4dfe37fb8a55032e595
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-23 20:06:15 +0100

    Add x icon and style it

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -2,13 +2,15 @@ import TopMenuItems from "./TopMenuItems"
 
 const BarsMenu = () =>{
     const styles={
-        container: `flex justify-center fixed top-0 bottom-0 right-0 left-0 animate-barsMenu z-[2] 
-        bg-white md:hidden `,
+        container: `flex flex-col  fixed top-0 bottom-0 right-0 left-0 animate-barsMenu z-[2] 
+        bg-white md:hidden text-[2.3rem] p-[3rem_5rem]`,
         topMenuItems:`flex items-center flex-col gap-[4rem] font-[600] mt-[15rem]
-        text-[2rem]`
+        `,
+        xIconContainer: ` self-end text-[2.5rem]`
     }
     return (
         <div className={styles.container}>
+            <span className={styles.xIconContainer}><i className='fa-solid fa-x'></i></span>
             <TopMenuItems containerClassName={styles.topMenuItems}/>
         </div>
     )
