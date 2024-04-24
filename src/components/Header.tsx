315a3cff6cb81370ee4548121e76c900b469b299
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-24 16:22:53 +0100

    Implement barsMenuController function that update isOpen state when called

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,10 @@ import TopMenuItems from "./TopMenuItems"
 
 const Header = ()=>{
     const [isOpen, setIsOpen] = useState(false)
+    const barsMenuController = ()=>{
+        setIsOpen(!isOpen)
+    }
+
     return(
         <header className="flex justify-between h-[80px] px-[50px] shadow-[0_0_10px_rgba(0,0,0,.09)]">
             <h1 className="flex items-center text-3xl font-bold ">
