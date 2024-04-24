71c6d40a96134c1ee00e2b57ad3970f3b37b2128
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-24 16:30:25 +0100

    Add isOpen prop and set right and left values based on it

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -1,9 +1,14 @@
 import TopMenuItems from "./TopMenuItems"
 
-const BarsMenu = () =>{
+type BarsMenuProps = {
+    isOpen: boolean
+}
+
+const BarsMenu = ({isOpen}:BarsMenuProps) =>{
     const styles={
-        container: `flex flex-col  fixed top-0 bottom-0 right-0 left-0 animate-barsMenu z-[2] 
-        bg-white md:hidden text-[2.3rem] p-[3rem_5rem]`,
+        container: `flex flex-col  fixed top-0 bottom-0 
+        ${isOpen? 'right-0 left-0':'right-[100%] left-[-100%]'}
+        z-[2] bg-white md:hidden text-[2.3rem] p-[3rem_5rem]`,
         topMenuItems:`flex items-center flex-col gap-[4rem] font-[600] mt-[15rem]
         `,
         xIconContainer: ` self-end text-[2.5rem]`
