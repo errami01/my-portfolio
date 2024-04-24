0fe8f81dc6542054dbaff458747a4fc29a21f1eb
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-24 16:31:18 +0100

    Add transition to the BarsMenu container

--- src/components/BarsMenu.tsx
+++ src/components/BarsMenu.tsx
@@ -7,8 +7,8 @@ type BarsMenuProps = {
 const BarsMenu = ({isOpen}:BarsMenuProps) =>{
     const styles={
         container: `flex flex-col  fixed top-0 bottom-0 
-        ${isOpen? 'right-0 left-0':'right-[100%] left-[-100%]'}
-        z-[2] bg-white md:hidden text-[2.3rem] p-[3rem_5rem]`,
+        ${isOpen? 'right-0 left-0':'right-[100%] left-[-100%]'} transition-all
+        duration-1000 z-[2] bg-white md:hidden text-[2.3rem] p-[3rem_5rem]`,
         topMenuItems:`flex items-center flex-col gap-[4rem] font-[600] mt-[15rem]
         `,
         xIconContainer: ` self-end text-[2.5rem]`
