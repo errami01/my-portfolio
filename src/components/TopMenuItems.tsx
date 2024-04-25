606d6566e0af1ff8c85337897924dbd3b15796ca
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-25 16:08:52 +0100

    Add handleMenuItemClick prop and pass it to the menu items as the onClick event handler

--- src/components/TopMenuItems.tsx
+++ src/components/TopMenuItems.tsx
@@ -1,15 +1,16 @@
 type TopMenuItemsProps ={
     containerClassName: string
+    handleMenuItemClick?: (isOpen:boolean)=>void
 }
-const TopMenuItems = ({containerClassName}:TopMenuItemsProps)=>{
+const TopMenuItems = ({containerClassName, handleMenuItemClick=()=>null}:TopMenuItemsProps)=>{
     const styles ={
         link: `flex items-center hover:text-[#4A86F7]`
     }
     return(
-      <div className={containerClassName}>
-        <a href="#hero" className={styles.link}>Home</a>
-        <a href="#projects" className={styles.link}>Projects</a>
-        <a href="#contact" className={styles.link}>contact</a>
+      <div className={`${containerClassName} bg-red-100`}>
+        <a href="#hero" className={styles.link} onClick={()=>handleMenuItemClick(false)}>Home</a>
+        <a href="#projects" className={styles.link} onClick={()=>handleMenuItemClick(false)}>Projects</a>
+        <a href="#contact" className={styles.link} onClick={()=>handleMenuItemClick(false)}>contact</a>
       </div>
     )
 }
