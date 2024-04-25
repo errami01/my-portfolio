c645578701bc5e38947be0bb5c155057554d92b6
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-25 19:42:21 +0100

    Set the header position to sticky and set its background color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,8 @@ const Header = ()=>{
     }
 
     return(
-        <header className="flex justify-between h-[80px] px-[50px] shadow-[0_0_10px_rgba(0,0,0,.09)]">
+        <header className="sticky top-0 flex justify-between h-[80px] px-[50px] 
+        shadow-[0_0_10px_rgba(0,0,0,.09)] bg-slate-100">
             <h1 className="flex items-center text-3xl font-bold ">
                 Logo
             </h1>
