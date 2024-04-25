0c559bc5459c5f6d7a08df2ba68eaf3346095454
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-04-25 19:50:53 +0100

    Remove z-index from BarsMenu container and add it to the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = ()=>{
 
     return(
         <header className="sticky top-0 flex justify-between h-[80px] px-[50px] 
-        shadow-[0_0_10px_rgba(0,0,0,.09)] bg-slate-100">
+        shadow-[0_0_10px_rgba(0,0,0,.09)] bg-slate-100 z-10">
             <h1 className="flex items-center text-3xl font-bold ">
                 Logo
             </h1>
