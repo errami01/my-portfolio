c5574f07f918a14f54dd19f0c9a03c4fcaf128ad
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-02 11:41:58 +0100

    Change logo text and add pointer cursor to it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,8 +11,8 @@ const Header = ()=>{
     return(
         <header className="sticky top-0 flex justify-between h-[80px] px-[50px] 
         shadow-[0_0_10px_rgba(0,0,0,.09)] bg-slate-100 z-10">
-            <h1 className="flex items-center text-3xl font-bold ">
-                Erramidev.com
+            <h1 className="flex items-center text-3xl font-bold cursor-pointer">
+                Erramidev.xyz
             </h1>
             <div className="flex  text-[1.7rem]">
                 <i 
