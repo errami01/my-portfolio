a0b9f7d5948290374608f8f78bb678b6296050cf
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-28 23:28:13 +0100

    Add styles for iconAndName container

--- src/components/SkillsGroup.tsx
+++ src/components/SkillsGroup.tsx
@@ -9,11 +9,13 @@ const SkillsGroup = ({title, skills}: SkillsGroupProps)=>{
         md:flex-row md:justify-start md:items-center`,
         techStack:`text-[1.7rem] py-[1rem] w-[10rem] font-semibold mb-[3rem] border-b-[0.1rem] 
         border-black md:mb-0 md:mr-[3rem] md:border-r-[0.1rem] md:border-b-0 pr-[1rem]`,
-        iconsContainer:`flex gap-[3rem]`
+        iconsContainer:`flex gap-[3rem]`,
+        iconAndName: `flex flex-col items-center gap-[0.5rem]`,
+        skillName: `text-[1.2rem] text-gray-500 font-medium`,
     }
-    const skillImages = skills.map(skill =><div key={nanoid()}>
+    const skillImages = skills.map(skill =><div key={nanoid()} className={styles.iconAndName}>
         <img src={`/assets/skill_icons/${skill}.svg`} width={50} />
-        <p>{skill}</p>
+        <p className={styles.skillName}>{skill}</p>
     </div>)
     return(
         <div className={styles.container}>
