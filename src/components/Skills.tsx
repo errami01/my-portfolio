ad913f2153e78b1b5d8075b261d86082e6e0691e
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-28 21:50:01 +0100

    Create Skills component

---
+++ src/components/Skills.tsx
@@ -0,0 +1,20 @@
+import Section from "./Section"
+import SectionTitle from "./SectionTitle"
+import SkillsGroup from "./SkillsGroup"
+
+const Skills = ()=>{
+    const frontEndSkills: string[] = ['HTML', 'CSS', 'JavaScript', 'TypeScript']
+    const backEndSkills: string[] = ['NodeJS-Dark']
+    const devOpsSkills: string[] = ['Git']
+    return(
+        <Section id="skills">
+            <SectionTitle text="Skills"/>
+            <SkillsGroup title="Front-end" skills={frontEndSkills}></SkillsGroup>
+            <SkillsGroup title="Back-end" skills={backEndSkills}></SkillsGroup>
+            <SkillsGroup title="DevOps" skills={devOpsSkills}></SkillsGroup>
+        </Section>
+
+        
+    )
+}
+export default Skills
