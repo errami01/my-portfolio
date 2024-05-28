3416cca2b2433db9800b29f17203b7865e809182
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-28 21:51:41 +0100

    Add Skills section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,12 +1,14 @@
 import Contact from "./Contact"
 import Hero from "./Hero"
 import Projects from "./Projects"
+import Skills from "./Skills"
 
 const Body = ()=>{
     
     return(
         <div className="px-8 max-w-[107rem] m-auto flex flex-col gap-[10rem] my-[6rem]">
            <Hero /> 
+           <Skills />
            <Projects />
            <Contact />
         </div>
