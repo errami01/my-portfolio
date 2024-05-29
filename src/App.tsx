1154cf826d433db6027ebbdbb6330f3a418443fd
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-05-29 13:01:38 +0100

    Add break word to contact info p element

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Header from './components/Header'
 import Body from './components/Body'
 import Footer from './components/Footer'
--- src/components/ContactBox.tsx
+++ src/components/ContactBox.tsx
@@ -4,14 +4,14 @@ type ContactBoxProps ={
 }
 const ContactBox = ({fontAwsomeClass, contactInfo}: ContactBoxProps)=>{
     const styles = {
-        icon:`text-[4rem] text-[#4A86F7]`,
         contactBoxContainer: `flex bg-white grow gap-[3rem] rounded-[1rem] p-[3rem] items-center 
-        text-[1.8rem] md:flex-col font-[500] text-[#555]`
+        text-[1.8rem] md:flex-col font-[500] text-[#555]`,
+        icon:`text-[4rem] text-[#4A86F7]`
     }
     return(
         <div className={styles.contactBoxContainer}>
             <i className={`${fontAwsomeClass} ${styles.icon}`}></i>
-            <p>{contactInfo}</p>
+            <p className="break-all ">{contactInfo}</p>
         </div>    
     )
 }
